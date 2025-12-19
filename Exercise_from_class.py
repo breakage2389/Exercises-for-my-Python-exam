bb248da33563dd05ba30c76c1e2c454f76f682ac
niksata <niki.kolev2006@gmail.com>
2025-12-19 11:50:55 +0200

    did the n 2nd time

--- Exercise_from_class.py
+++ Exercise_from_class.py
@@ -1,10 +1,12 @@
 while True:
     try:
-        number = int(input("15 < n < 35: "))
-        if 15 < number < 35:
+        n = int(input("Enter a number: "))
+        if not (15 < n < 35):
+            print("Invalid input |15 < n <35|")
+            continue
+        else:
             break
-        print("Number must be between 15 and 35")
     except ValueError:
-        print("Enter a valid integer")
-
-print(number)
+        print("Invalid input")
+    except ArithmeticError:
+        print("Invalid input -> arithmetic error")
