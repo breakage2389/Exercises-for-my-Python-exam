03176cb8cca48c6327ab86615d72ea1d7f3f9a2f
niksata <niki.kolev2006@gmail.com>
2025-12-20 10:24:46 +0200

    2nd exercise

--- Exercise_from_class.py
+++ Exercise_from_class.py
@@ -55,47 +55,82 @@
 
 
 # 2ra zad
-# class Worker:
-#     def __init__(self,worker_num , fname , lname , work_experience_company , total_years_experience , salary , age):
-#         self.worker_num = worker_num
-#         self.fname = fname
-#         self.lname = lname
-#         self.work_experience_company = work_experience_company
-#         self.total_years_experience = total_years_experience
-#         self.salary = salary
-#         self.age = age
-#
-#     def worker_information(self):
-#         print("Worker's Information")
-#         print(f'Workers number/id :{self.worker_num}')
-#         print("Worker's Name : ", self.fname)
-#         print("Worker's last name : ", self.lname)
-#         print("Worker's Working Experience Company : ", self.work_experience_company)
-#         print("Worker's Total Years Experience : ", self.total_years_experience)
-#         print("Worker's Salary : ", self.salary)
-#         print("Worker's Age : ", self.age)
-#
-#     def salary_bonus(self):
-#         print("Salary bonus")
-#         bonus = 0
-#         if self.work_experience_company > 10:
-#             bonus = self.salary * 2/100
-#         elif 5 <= self.work_experience_company <= 10:
-#             bonus= self.salary * 1.5/100
-#         elif self.work_experience_company < 5:
-#             bonus= self.salary * 0.5/100
-#         print("Salary bonus : ", bonus)
-#         return bonus
+class Worker:
+    def __init__(self,worker_num , fname , lname , work_experience_company , total_years_experience , salary , age):
+        self.worker_num = worker_num
+        self.fname = fname
+        self.lname = lname
+        self.work_experience_company = work_experience_company
+        self.total_years_experience = total_years_experience
+        self.salary = salary
+        self.age = age
+
+    def worker_information(self):
+        print()
+        print("Worker's Information")
+        print(f'Workers number/id :{self.worker_num}')
+        print("Worker's Name : ", self.fname)
+        print("Worker's last name : ", self.lname)
+        print("Worker's Working Experience Company : ", self.work_experience_company)
+        print("Worker's Total Years Experience : ", self.total_years_experience)
+        print("Worker's Salary : ", self.salary)
+        print("Worker's Age : ", self.age)
+
+    def salary_bonus(self):
+        print("Salary bonus")
+        bonus = 0
+        if self.work_experience_company > 10:
+            bonus = self.salary * 2/100
+        elif 5 <= self.work_experience_company <= 10:
+            bonus= self.salary * 1.5/100
+        elif self.work_experience_company < 5:
+            bonus= self.salary * 0.5/100
+        print("Salary bonus : ", bonus)
+        return bonus
 
 
 workers_list = []
 n = int(input("Enter the number of workers : "))
 for i in range(n):
-    print("Enter worker number ", i + 1)
-    work_number = int(input('Enter the worker number : '))
+    worker_num= int(input("Enter the worker number : "))
     fname = input("Enter first name: ")
     lname = input("Enter last name: ")
-    work_experience_company = input("Enter experience in the company: ")
-    total_work_experience = input("Enter total work experience: ")
+    work_experience_company = int(input("Enter experience in the company: "))
+    total_work_experience = int(input("Enter total work experience: "))
     salary = int(input("Enter salary: "))
     age = int(input("Enter age: "))
+
+    workers_list.append(
+        Worker(worker_num , fname , lname, work_experience_company, total_work_experience, salary, age)
+    )
+
+def search_by_num(num, workers_list):
+    for worker in workers_list:
+        if worker.worker_num == num:
+            return True
+    return False
+
+def search_by_name_experience(workers_list , name , experience):
+    return [worker for worker in workers_list if worker.fname.lower()== name.lower() and
+           worker.work_experience_company == experience ]
+
+def avg_worker_age(workers_list):
+    count = 0
+    age_container = 0
+    for i in workers_list:
+        if i.work_experience_company > 10:
+            count += 1
+            age_container += i.age
+        if i.age == 0:
+            return 0
+    return age_container / count
+
+def remove_worker(workers_list , work_num):
+    for i in workers_list:
+        if i.worker_num == work_num:
+            print('Information deleted')
+            workers_list.remove(i)
+        else:
+            print('Wrong work_num')
+
+print(avg_worker_age(workers_list))
