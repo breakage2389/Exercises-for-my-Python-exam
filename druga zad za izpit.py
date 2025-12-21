d73bf8c1a4846643d6daf080197314e59cc9b2ea
niksata <niki.kolev2006@gmail.com>
2025-12-21 19:39:37 +0200

    Everything is done

--- druga zad za izpit.py
+++ druga zad za izpit.py
@@ -37,13 +37,24 @@ for i in range(n):
 print(nums)
 
 for i in nums:
-    if i <= k:
+    if i <= k: #ako chislo e po-malko ot k go maha ili kakto e tuk ako i e po-malko ili ravno go dobavq
         nums1.append(i)
 print(nums1)
 
-nums1_uneven = [nums1[1::2]] #necheten index only
-storage  =1
-for num1 in nums1:
-    storage = 1 * num1
-
+nums1_uneven =nums1[1::2] #necheten index only
+storage = 1
+for num1 in nums1_uneven:
+    storage *= num1
 print(storage)
+
+#za namiraneto edin nachin e ako ordernem lista i nums1[0] vinagi shte bude nai-malkiq el
+# nums1.sort()
+# print(f'nai-malkiq el e {nums1[0]}')
+
+min_value = min(nums1)
+min_index = nums1.index(min_value)
+
+# list 2 -> nums2
+
+nums2 = [i for i in nums if i >0]
+print(f'Razlikata v nums2 e ->{max(nums2) - min(nums2)}')
