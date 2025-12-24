9c1e3b35709f3bf5cdfad6f4744b66cda2761f5b
niksata <niki.kolev2006@gmail.com>
2025-12-24 13:35:11 +0200

    z

---
+++ zadachatyp1.py
@@ -0,0 +1,72 @@
+#izpit po vuvedenie v programiraneto var 1
+#n = 25 <= n <= 45 // while True : -> try except prowerka
+#lst_1 = s 'n' na broi el ,  interval (p;q )
+# p e random int ot -3700 , -1600 ,  q = 2222 do  3333
+#broi na el >0 from lst_1 % 6 == 3
+#lst_2 posle
+
+import random
+
+# while True:
+#     try:
+#         n = int(input('25<= n <=45'))
+#         if 25 <= n <= 45:
+#             break
+#     except ValueError:
+#         print('Trqbwa da bude int  (25<= n <=45), gledai da ne e bukwa')
+#         continue
+#     except Exception as e:
+#         print(e)
+#         continue
+
+
+
+
+
+#tuk shte go prawq s n=5 zashto prowerkata shte bude muchenie.. :*(
+# n = 5
+# p = random.randint(-3700,-1600)
+# q = random.randint(2222 ,  3333)
+#
+# lst_1 = []
+# print(f'Intervala e {p} do {q} vkluchitelno')
+#
+# for i in range(n):
+#     i = int(input('dobawi chislo'))
+#     if p <= i <= q:
+#         lst_1.append(i)
+#     else:
+#         print(f'chislto trqbwa da e v interwala -> [{p} ; {q}]')
+#
+# print(lst_1)
+# count=0
+# for i in lst_1:
+#     if ((i //100)%10) % 2 == 0:
+#         count += 1
+# print(f'broq e ->{count}')
+#
+# min_value= float('inf')
+# min_index = -1
+# for i , y in enumerate(lst_1):
+#     if y % 6 ==3:
+#         if y < min_value:
+#             min_value = y
+#             min_index = i
+# print(min_index , min_value)
+#
+
+lst_1 = [-25, 21 ,4,5 , 25 ,75, 50 ,125]
+lst_2 = []
+for num in lst_1:
+    if 10<= num <= 99 or -99<= num <=-10:
+        if num % 5 ==0:
+            lst_2.append(num)
+print(lst_2)
+
+
+collector = 1
+odd_lst_2 = lst_2[1::2]
+for i in odd_lst_2:
+    collector *= i
+print(collector)
+
