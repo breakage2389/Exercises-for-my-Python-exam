5c286e55a62da1a4994d8913163908c684987d9f
niksata <niki.kolev2006@gmail.com>
2025-12-26 10:37:16 +0200

    10 zadachi 26.12.2025

--- list_comprehension_exercises.py
+++ list_comprehension_exercises.py
@@ -4,6 +4,7 @@
 #resheniee
 # prices = [10.50, 120.0, 45.0, 90.0, 5.20]
 # expensive_prices = [x for x in prices if x >50]
+from selectors import SelectSelector
 
 # 2. Трансформация на имена (Upper case)
 # Това е често срещано при обработка на данни от потребителски вход.
@@ -27,21 +28,21 @@
 # Задача: Използвай list comprehension, за да извлечеш само имената на продуктите,
 # чиято наличност (quantity) е по-малка от 10.
 #resheniee
-# class Market:
-#     def __init__(self, barcod, name, manufacturer, price, quantity):
-#         self.barcod = barcod
-#         self.name = name
-#         self.manufacturer = manufacturer
-#         self.price = price
-#         self.quantity = quantity
-#
-# nike_products = [
-#     Market(1001, "Air Max 270", "Nike", 280.00, 10),
-#     Market(1002, "Revolution 6", "Nike", 45.00, 25),
-#     Market(1003, "Downshifter 12", "Nike", 25.00, 3),  # < 10
-#     Market(1004, "Jordan Low", "Nike", 210.00, 5),     # < 10
-#     Market(1005, "Court Vision", "Nike", 35.00, 20)
-# ]
+class Market:
+    def __init__(self, barcod, name, manufacturer, price, quantity):
+        self.barcod = barcod
+        self.name = name
+        self.manufacturer = manufacturer
+        self.price = price
+        self.quantity = quantity
+
+nike_products = [
+    Market(1001, "Air Max 270", "Nike", 280.00, 10),
+    Market(1002, "Revolution 6", "Nike", 45.00, 25),
+    Market(1003, "Downshifter 12", "Nike", 25.00, 3),  # < 10
+    Market(1004, "Jordan Low", "Nike", 210.00, 5),     # < 10
+    Market(1005, "Court Vision", "Nike", 35.00, 20)
+]
 # l1 = [x.name for x in nike_products if x.quantity < 10 ]
 # for z in l1:
 #     print(z)
@@ -66,3 +67,80 @@
 #     for x in spisuk
 # ]
 # print(l1st)
+
+# 1. Филтър за дължина на думи
+# Вход: words = ["apple", "it", "python", "ai", "banana", "code"]
+# Задача: Създай списък само с думите, които са по-дълги от 3 символа.
+#reshenie
+# words = ["apple", "it", "python", "ai", "banana", "code"]
+# l1st = [x for x in words if len(x) >3]
+# print(l1st)
+
+# 2. Числата в стрингове
+# Вход: numbers = [1, 5, 10, 15]
+# Задача: Превърни списъка в стрингове, но добави "лв." след всяко число (напр. "1 лв.").
+# numbers = [1, 5, 10, 15]
+# l1st = [f'{str(x)}лв.' for x in numbers ]
+# print(l1st)
+
+# 3. Търсене на буква
+# Вход: fruits = ["apple", "cherry", "banana", "kiwi", "mango"]
+# Задача: Направи списък само от плодовете, които съдържат буквата "a".
+# fruits = ["apple", "cherry", "banana", "kiwi", "mango"]
+# l1st = [x for x in fruits if 'a' in x.lower()]
+# print(l1st)
+#################### po-trudni primeri
+# 4. Обратни числа
+# Вход: nums = [1, -2, 3, -4, 5]
+# Задача: Създай списък, в който положителните числа стават отрицателни, а отрицателните — положителни.
+# nums = [1, -2, 3, -4, 5]
+# l1st = [-x for x in nums]
+# print(l1st)
+
+# 5. Филтър "Кратни на 3 и 5"
+# Вход: range(1, 51)
+# Задача: Извлечи само числата, които се делят едновременно на 3 и на 5.
+# l1st = [x for x in range(1,51) if x % 3 == 0 if x % 5 == 0]
+# print(l1st)
+
+# 6. Намиране на индекси
+# Вход: letters = ["a", "b", "c", "d"]
+# Задача: Използвай enumerate() в list comprehension, за да получиш списък от тупли (индекс, елемент).
+# letters = ["a", "b", "c", "d"]
+# l1st = [(idx,el) for idx,el in enumerate(letters)]
+# print(l1st)
+
+################################ oshte po-trudni
+# 7. "Elif" категоризация на цени
+# Вход: prices = [5, 15, 60, 120, 8]
+# Задача: Създай списък със статуси: "Cheap" (под 10), "Normal" (10-50), "Expensive" (над 50).
+# prices = [5, 15, 60, 120, 8]
+# l1st = [
+#     'cheap' if x <10 else
+#     'normal' if 10<=x <= 50 else
+#     'expensive' for x in prices
+# ]
+# print(l1st)
+
+# 8. Филтриране на обекти по име
+# Вход: Използвай твоя списък nike_products.
+# Задача: Извлечи обектите (целите обекти!), чието име съдържа думата "Air".
+# l1st = [x for x in nike_products if 'Air' in x.name]
+# for i in l1st:
+#     print(i.barcod, i.name, i.quantity , i.price , i.quantity)
+
+
+# 9. Списък от речници
+# Вход: nike_products.
+# Задача: Създай списък, където всеки елемент е речник с ключове "n" (за името) и "p" (за цената).
+# Пример: [{"n": "Air Max", "p": 250}, ...]
+# l1st = [{'n' : x.name , 'p' : x.price} for x in nike_products]
+# for item in l1st:
+#     print(item)
+
+
+# 10. "Flatten" на матрица с филтър
+# Вход: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# Задача: Направи списък от всички числа в матрицата, но само ако са четни.
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# l1st = [num for row in matrix for num in row if num %2 ==0 ]
